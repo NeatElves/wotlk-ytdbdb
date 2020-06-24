f5c2af85baf741e5fdccac31ca146e2aa5e7b113
NeatElves <neatelves@gmail.com>
2020-06-24 08:56:13 +0300

    для ядра "Implement SetRootSelf and EAI corresponding command ACTION_T_SET_IMMOBILIZED_STATE"

--- Updates/0001_updatepack.sql
+++ Updates/0001_updatepack.sql
@@ -1890,3 +1890,13 @@ INSERT INTO `creature_movement_template` (`entry`, `pathId`, `point`, `position_
 (38339, 0, 2, 1764.49, -4345.93, -7.798, 100, 0, 1, NULL);
 
 UPDATE creature_template SET MovementType = 0 WHERE entry = 21628;
+
+DELETE FROM dbscripts_on_relay WHERE id = 10124;
+INSERT INTO dbscripts_on_relay (id, delay, priority, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES 
+(10124,0,0,15,42660,0,0,0,0,0,0,0,0,0,0,0,0,0,'Gavis Greyshield - Cast Gavis Greyshield Credit'),
+(10124,10,0,4,59,784,0,0,0,0,0,0,0,0,0,0,0,0,'Gavis Greyshield - Set UnitFlags 784'),
+(10124,100,0,22,84,1,0,0,0,0,0,0,0,0,0,0,0,0,'Gavis Greyshield - Set Faction 84'),
+(10124,1000,0,42,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'Gavis Greyshield - Set Equipment Slots'),
+(10124,1100,0,0,0,0,0,0,0,0,2000003430,0,0,0,0,0,0,0,'Gavis Greyshield - Say'),
+(10124,4000,0,0,0,0,0,0,0,0,2000003431,0,0,0,0,0,0,0,'Gavis Greyshield - Say'),
+(10124,15000,0,18,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'Gavis Greyshield - Despawn Self');
