8da9792bf66fba237e14d3f81cd705c260c482da
NeatElves <neatelves@gmail.com>
2022-12-31 20:37:22 +0200

    [0.15.2.0030.1]

--- utilities/4_cmangos_custom.sql
+++ utilities/4_cmangos_custom.sql
@@ -209,8 +209,3 @@ UPDATE gameobject_template SET data1 = 2 WHERE entry IN (188539);
 
 -- Wolfsbane Root 189313 - must despawn
 UPDATE gameobject_template SET data5 = 1 WHERE entry IN (189313);
-
--- Dalaran book: make it despawnable on use with autoclose value (autoclose = autoCloseTime / IN_MILLISECONDS (prior to 3.0.3, conversion was / 0x10000))
-UPDATE gameobject_template SET data3=180000, data5=1 WHERE entry IN (192651,192652,192653,192706,192707,192708,192709,192710,192711,192713,192865,192866,192867,192868,192869,192870,192871,192872,192874,192880,192881,192882,192883,192884,192885,192886,192887,192888,192889,192890,192891,192894,192895,192896,192905);
--- Felwood corrupted *.flower: make it despawnable on use with autoclose value (autoclose = autoCloseTime / IN_MILLISECONDS (prior to 3.0.3, conversion was / 0x10000))
-UPDATE gameobject_template SET data3=175000, data5=1 WHERE entry IN (164881,164882,164883,164884,171940,171943,173325,173326,174609,174610,174612,174613,174614,174615,174616,174617,174618,174619,174620,174621,174622,174623,174624,174625,174685,174687,174710,174711,174714,174715);
