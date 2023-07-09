387e45ecad438aa6e6a028206dc1e3f40529a62b
NeatElves <neatelves@gmail.com>
2023-07-09 21:34:19 +0300

    [0.15.3.0006]

--- Updates/0003_BroadcastTextUpdate.sql
+++ Updates/0003_BroadcastTextUpdate.sql
@@ -5435,3 +5435,38 @@ UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10544 WHERE Id IN(17779);
 UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10545 WHERE Id IN(17778);
 UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10540 WHERE Id IN(17783);
 UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10546 WHERE Id IN(17777);
+
+UPDATE broadcast_text SET ChatTypeID=0,EmoteId1=0 WHERE Id IN(17246);
+UPDATE broadcast_text SET ChatTypeID=0,EmoteId1=0 WHERE Id IN(17248);
+UPDATE broadcast_text SET ChatTypeID=0,EmoteId1=28 WHERE Id IN(17249);
+UPDATE broadcast_text SET ChatTypeID=0,EmoteId1=0 WHERE Id IN(17251);
+UPDATE broadcast_text SET ChatTypeID=0,EmoteId1=0 WHERE Id IN(17252);
+UPDATE broadcast_text SET ChatTypeID=0,EmoteId1=0 WHERE Id IN(17254);
+UPDATE broadcast_text SET ChatTypeID=0,EmoteId1=28 WHERE Id IN(17255);
+UPDATE broadcast_text SET ChatTypeID=0,EmoteId1=0 WHERE Id IN(17256);
+UPDATE broadcast_text SET ChatTypeID=0,EmoteId1=0 WHERE Id IN(17258);
+UPDATE broadcast_text SET ChatTypeID=0,EmoteId1=0 WHERE Id IN(17260);
+UPDATE broadcast_text SET ChatTypeID=0,EmoteId1=0 WHERE Id IN(17261);
+UPDATE broadcast_text SET ChatTypeID=1,EmoteId1=0 WHERE Id IN(17241);
+UPDATE broadcast_text SET ChatTypeID=0,EmoteId1=1 WHERE Id IN(17243);
+UPDATE broadcast_text SET ChatTypeID=0,EmoteId1=1 WHERE Id IN(17263);
+UPDATE broadcast_text SET ChatTypeID=0,EmoteId1=0 WHERE Id IN(17244);
+UPDATE broadcast_text SET ChatTypeID=0,EmoteId1=1 WHERE Id IN(17264);
+UPDATE broadcast_text SET ChatTypeID=0,EmoteId1=1 WHERE Id IN(17265);
+UPDATE broadcast_text SET ChatTypeID=2,EmoteId1=0 WHERE Id IN(17228);
+UPDATE broadcast_text SET ChatTypeID=0,EmoteId1=1 WHERE Id IN(17229);
+UPDATE broadcast_text SET ChatTypeID=2,EmoteId1=28 WHERE Id IN(17230);
+UPDATE broadcast_text SET ChatTypeID=0,EmoteId1=1 WHERE Id IN(17231);
+
+-- 4826_Avalanche_SholazarBasin.sql
+UPDATE broadcast_text SET ChatTypeID = 1 WHERE Id IN (27537,27538,27539);
+
+-- 0101_ikis_bct.sql TBC
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10557 WHERE Id IN(17769);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10554 WHERE Id IN(17765);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10555 WHERE Id IN(17767);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10556 WHERE Id IN(17768);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10558 WHERE Id IN(17763);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10559 WHERE Id IN(17764);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10560 WHERE Id IN(17762);
+UPDATE broadcast_text SET ChatTypeID=3 WHERE Id IN(19738);
