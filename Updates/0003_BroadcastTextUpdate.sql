e6790ff92e6fd0908ccf4c11eac168e39f9ff073
NeatElves <neatelves@gmail.com>
2023-07-16 22:58:14 +0300

    [0.15.3.0007.2]

--- Updates/0003_BroadcastTextUpdate.sql
+++ Updates/0003_BroadcastTextUpdate.sql
@@ -5479,3 +5479,12 @@ UPDATE broadcast_text SET ChatTypeID=1, SoundEntriesID1=10363 WHERE Id IN(17698)
 UPDATE broadcast_text SET ChatTypeID=1, SoundEntriesID1=10364 WHERE Id IN(17701);
 UPDATE broadcast_text SET ChatTypeID=1, SoundEntriesID1=10365 WHERE Id IN(17702);
 UPDATE broadcast_text SET ChatTypeID=1, SoundEntriesID1=10366 WHERE Id IN(17704);
+
+-- 0120_sv_kalithresh.sql TBC
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10390 WHERE Id IN(17724);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10391 WHERE Id IN(17725);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10392 WHERE Id IN(17726);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10393 WHERE Id IN(17727);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10394 WHERE Id IN(17728);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10395 WHERE Id IN(17729);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=10397 WHERE Id IN(17730);
--- Updates/0004_updatepack.sql
+++ Updates/0004_updatepack.sql
@@ -11872,4 +11872,15 @@ INSERT INTO `spell_script_target` (`entry`, `type`, `targetEntry`, `inverseEffec
 INSERT INTO `spell_script_target` (`entry`, `type`, `targetEntry`, `inverseEffectMask`) VALUES ('18997', '1', '1840', '0');
 INSERT INTO `spell_script_target` (`entry`, `type`, `targetEntry`, `inverseEffectMask`) VALUES ('20001', '1', '12581', '0');
 
+UPDATE `item_template` SET `MinMoneyLoot`=47, `MaxMoneyLoot`=302 WHERE `entry`=9276;
+UPDATE `item_loot_template` SET `groupid` = '1' WHERE `entry` =9276 AND `item` =9249;
+UPDATE `item_loot_template` SET `ChanceOrQuestChance` = '15',`groupid` = '1' WHERE `entry` =9276 AND `item` =9250;
+
+UPDATE creature_spell_list SET Flags=0 WHERE Id IN(1593201) AND SpellId IN(54427,28374);
+UPDATE creature_spell_list SET Flags=0 WHERE Id IN(1595301) AND SpellId IN(28796,28794,28798);
+UPDATE creature_spell_list SET Flags=0 WHERE Id IN(1595601) AND SpellId IN(28783,28785);
+UPDATE creature_spell_list SET Flags=0 WHERE Id IN(2930601,3136801,2924901,2926801,2941701,1598901,1598902,1601101,1601102,1602802,1606101,2835701,2971801,2971802,2999101,2999102);
+UPDATE creature_spell_list_entry a SET ChanceSupportAction=0 WHERE NOT EXISTS(SELECT Id FROM creature_spell_list b WHERE a.Id=b.Id AND b.Flags&1);
+UPDATE creature_spell_list_entry a SET ChanceRangedAttack=0 WHERE NOT EXISTS(SELECT Id FROM creature_spell_list b WHERE a.Id=b.Id AND b.Flags&2);
+
 -- --------------------------------------------------------------------------------------------------------------------------------------------------------------
