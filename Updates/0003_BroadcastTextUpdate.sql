1fb5d6949df04b98ae134945b117a8673ec31f77
NeatElves <neatelves@gmail.com>
2024-01-20 11:05:56 +0200

    [0.15.3.0024.6]

--- Updates/0003_BroadcastTextUpdate.sql
+++ Updates/0003_BroadcastTextUpdate.sql
@@ -6349,3 +6349,67 @@ UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=15571 WHERE Id=33871;
 UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=15572 WHERE Id=34313;
 UPDATE broadcast_text SET ChatTypeID=3 WHERE Id=33465;
 UPDATE broadcast_text SET ChatTypeID=2 WHERE Id=34471;
+
+-- 0346_maiden_of_virtue.sql TBC
+-- Karazhan | Maiden of Virtue bct
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=9204,EmoteId1=0 WHERE Id IN(15079);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=9313,EmoteId1=0 WHERE Id IN(15080);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=9208,EmoteId1=0 WHERE Id IN(13396);
+UPDATE broadcast_text SET ChatTypeID=1,SoundEntriesID1=9206,EmoteId1=0 WHERE Id IN(15081);
+
+-- 0364_shattered_halls_bct.sql TBC
+-- Add Sound Entries and correct some broadcast_texts
+-- Figther Down!
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10172' WHERE Id = '16356';
+-- Replacement, quickly!
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10173' WHERE Id = '16357';
+-- Next warrior, now!
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10174' WHERE Id = '16358';
+-- Fall in! Mok-thora ka!
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10175' WHERE Id = '16359';
+-- Where is my Support?
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10176' WHERE Id = '16360';
+-- Next warrior, step up!
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10180' WHERE Id = '16361';
+-- Join the fight! Agrama-ka!
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10181' WHERE Id = '16362';
+
+
+-- Wake up, we're under attack! 16363
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10182' WHERE Id = '16363';
+-- Sleep on your own time! 16364
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10183' WHERE Id = '16364';
+-- Get up! 16365
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10184' WHERE Id = '16365';
+-- On your feet! 16366
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10185' WHERE Id = '16366';
+-- No time for slumber! Join the fight! 16367
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10186' WHERE Id = '16367';
+
+
+-- Get ready! This shouldn't take long...
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10189' WHERE Id = '16349';
+
+-- Show them no quarter! Form up!
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10191' WHERE Id = '16350';
+
+-- Line up and crush these fools!
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10187' WHERE Id = '16346';
+
+-- Form ranks and make the intruders pay!
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10190' WHERE Id = '17461';
+
+-- Form up! Let's make quick work of them!
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10188' WHERE Id = '16347';
+
+-- Gakarah ma!
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10194' WHERE Id = '16354';
+
+-- We must halt their advance! Take your positions!
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10196' WHERE Id = '16355';
+
+-- Lok-Narash! Defensive positions!
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10192' WHERE Id = '16352';
+
+-- Hold them back at all costs!
+UPDATE broadcast_text SET ChatTypeID='1', SoundEntriesID1='10195' WHERE Id = '17462';
