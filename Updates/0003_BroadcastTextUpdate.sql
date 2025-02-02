1e5d271821ca2e8c99d4981bda17861e7a7b227f
NeatElves <neatelves@gmail.com>
2025-02-02 10:21:20 +0200

    [0.15.3.0037.1]

--- Updates/0003_BroadcastTextUpdate.sql
+++ Updates/0003_BroadcastTextUpdate.sql
@@ -6702,3 +6702,22 @@ UPDATE broadcast_text SET ChatTypeID = '1' WHERE Id = '14865';
 -- 5548c_TDB-0394_bota_cleanup.sql
 -- Text should be TextEmote
 UPDATE broadcast_text SET ChatTypeID = '2' WHERE Id = '16944';
+
+-- 5578_TDB-0445_magtheridon_cleanup.sql
+-- Broadcast Text corrections
+
+UPDATE `broadcast_text` SET `ChatTypeID`='1', `SoundEntriesID1`='10253' WHERE (`Id`='17346');
+UPDATE `broadcast_text` SET `ChatTypeID`='1', `SoundEntriesID1`='10256' WHERE (`Id`='17348');
+UPDATE `broadcast_text` SET `ChatTypeID`='1', `SoundEntriesID1`='10257' WHERE (`Id`='17336');
+UPDATE `broadcast_text` SET `ChatTypeID`='1', `SoundEntriesID1`='10255' WHERE (`Id`='17349');
+UPDATE `broadcast_text` SET `ChatTypeID`='1', `SoundEntriesID1`='10258' WHERE (`Id`='17347');
+UPDATE `broadcast_text` SET `ChatTypeID`='3' WHERE (`Id`='18739');
+UPDATE `broadcast_text` SET `ChatTypeID`='2' WHERE (`Id`='13691');
+UPDATE `broadcast_text` SET `ChatTypeID`='2' WHERE (`Id`='13689');
+UPDATE `broadcast_text` SET `ChatTypeID`='2' WHERE (`Id`='13690');
+UPDATE `broadcast_text` SET `ChatTypeID`='6', `SoundEntriesID1`='10247' WHERE (`Id`='17339');
+UPDATE `broadcast_text` SET `ChatTypeID`='6', `SoundEntriesID1`='10248' WHERE (`Id`='17340');
+UPDATE `broadcast_text` SET `ChatTypeID`='6', `SoundEntriesID1`='10249' WHERE (`Id`='17341');
+UPDATE `broadcast_text` SET `ChatTypeID`='6', `SoundEntriesID1`='10250' WHERE (`Id`='17342');
+UPDATE `broadcast_text` SET `ChatTypeID`='6', `SoundEntriesID1`='10251' WHERE (`Id`='17343');
+UPDATE `broadcast_text` SET `ChatTypeID`='6', `SoundEntriesID1`='10252' WHERE (`Id`='17344');
